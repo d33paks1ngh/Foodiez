4124aadb003c208f7f4cf6eb02167022e20bea39
Deepak Singh <ds8853845715@gmail.com>
2024-12-16 11:25:19 +0530

    cors solved at local

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,11 +2,11 @@ const CDN_URL =
   "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
 const HOME_PAGE_URL =
-  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.6914766&lng=77.45818609999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+  "https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.6914766&lng=77.45818609999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
 const MENU_API =
-  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.6914766&lng=77.45818609999999&restaurantId=";
+  "https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.6914766&lng=77.45818609999999&restaurantId=";
 
 const RES_LIST =
-  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.6914766&lng=77.45818609999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+  "https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.6914766&lng=77.45818609999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
 
 export { CDN_URL, HOME_PAGE_URL, MENU_API, RES_LIST };
